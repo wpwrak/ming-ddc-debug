7e88e8e841bd266cffa45655263f75b61fa880ff
Werner Almesberger <werner@almesberger.net>
2013-04-12 17:18:16 -0300

    use EDID class from milkymist-ng again

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
+MI_NG=$(shell pwd)/../milkymist-ng
+
 .PHONY:		all run poll
 
 all:		build/top.bit
 
-build/top.bit:	build.py edid.py
-#		PYTHONPATH=$$PYTHONPATH:`pwd`/../milkymist-ng
-		python3 ./build.py
+build/top.bit:	build.py $(MI_NG)/milkymist/dvisampler/edid.py
+		PYTHONPATH=$$PYTHONPATH:$(MI_NG) python3 ./build.py
 
 run:		build/top.bit
 		( echo cable milkymist; echo detect; \
--- build.py
+++ build.py
@@ -2,12 +2,12 @@ from migen.fhdl.structure import *
 from migen.fhdl.module import Module
 from mibuild.platforms import m1
 
-from edid import EDID
+from milkymist.dvisampler.edid import EDID
 
 class EDIDTester(Module):
 	def __init__(self, clk50_pad, rst_pad, dvi_pads, dbg_pads, led_pad,
 	    unused_pads):
-		self.comb += unused_pads.eq(0)
+#		self.comb += unused_pads.eq(0)
 		self.clock_domains.cd_sys = ClockDomain("sys")
 		self.clock_domains.cd_pix = ClockDomain("pix")
 		self.comb += [
@@ -16,7 +16,7 @@ class EDIDTester(Module):
 			self.cd_pix.clk.eq(dvi_pads.clk)
 		]
 
-		self.submodules.edid = EDID(dvi_pads, dbg_pads)
+		self.submodules.edid = EDID(dvi_pads)
 
 		counter = Signal(23)
 		self.comb += led_pad.eq(counter[22])
@@ -25,8 +25,12 @@ class EDIDTester(Module):
 plat = m1.Platform()
 dvi_pads = plat.request("dvi_in", 0)
 mmc = plat.request("mmc", 0)
-dbg_pads = Cat(*(mmc.dat[2], mmc.cmd, mmc.clk, mmc.dat[1]))
-unused_pads = Cat(*(mmc.dat[0], mmc.dat[3]))
+if False:
+	dbg_pads = Cat(*(mmc.dat[2], mmc.cmd, mmc.clk, mmc.dat[1]))
+	unused_pads = Cat(*(mmc.dat[0], mmc.dat[3]))
+else:
+	dbg_pads = None
+	unused_pads = None
 dut = EDIDTester(plat.request("clk50"), plat.request("user_btn", 0),
 	dvi_pads, dbg_pads, plat.request("user_led", 0), unused_pads)
 plat.add_platform_command("NET \"{clk}\" CLOCK_DEDICATED_ROUTE = FALSE;", clk=dvi_pads.clk)
