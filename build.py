ede2f733dd6c4d4ff65395788d61c0dbcea4c91a
Werner Almesberger <werner@almesberger.net>
2013-04-11 21:40:23 -0300

    build.py: revert counter on MMC (we'll need the pins for edid.py)
    This reverts commit c51bcebc36df95c123dec3cb3c6ff2ceb3d2c630.

--- build.py
+++ build.py
@@ -5,8 +5,7 @@ from mibuild.platforms import m1
 from edid import EDID
 
 class EDIDTester(Module):
-	def __init__(self, clk50_pad, rst_pad, dvi_pads, led_pad,
-	    x0, x1, x2, x3, x4, x5):
+	def __init__(self, clk50_pad, rst_pad, dvi_pads, led_pad):
 		self.clock_domains.cd_sys = ClockDomain("sys")
 		self.clock_domains.cd_pix = ClockDomain("pix")
 		self.comb += [
@@ -20,17 +19,10 @@ class EDIDTester(Module):
 		counter = Signal(23)
 		self.comb += led_pad.eq(counter[22])
 		self.sync.pix += counter.eq(counter + 1)
-		self.comb += Cat(*(x0, x1, x2, x3, x4, x5)).eq(counter[10:16])
-#		self.comb += x0.eq(counter[10])
-#		self.comb += x1.eq(counter[12])
-#		self.comb += x2.eq(counter[14])
-#		self.comb += x3.eq(counter[16])
 
 plat = m1.Platform()
 dvi_pads = plat.request("dvi_in", 0)
-x = plat.request("mmc", 0)
 dut = EDIDTester(plat.request("clk50"), plat.request("user_btn", 0),
-	dvi_pads, plat.request("user_led", 0),
-	x.dat[1], x.clk, x.cmd, x.dat[3], x.dat[2], x.dat[0])
+	dvi_pads, plat.request("user_led", 0))
 plat.add_platform_command("NET \"{clk}\" CLOCK_DEDICATED_ROUTE = FALSE;", clk=dvi_pads.clk)
 plat.build_cmdline(dut)
